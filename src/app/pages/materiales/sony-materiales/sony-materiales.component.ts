6a1655b2bad0069036933cd3911795b70c55fb5e
cristian97r <cristian97drb@gmail.com>
2019-12-19 14:19:10 -0400

    Add function to filter by materials and theaters
    small cursor fix on movies components

--- src/app/pages/materiales/sony-materiales/sony-materiales.component.ts
+++ src/app/pages/materiales/sony-materiales/sony-materiales.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { DataApiService } from '../../../services/data-api.service';
-import { NgForm } from '@angular/forms';
-import { AuthService } from '../../../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { UserInterface } from '../../../models/users';
-import { map } from 'rxjs/operators';
-import { MaterialesSonyInterface, MaterialSonyInterface } from 'src/app/models/materialesSony';
-import { NgbCarousel,NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { DataApiService } from "../../../services/data-api.service";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "../../../services/auth.service";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { UserInterface } from "../../../models/users";
+import { map } from "rxjs/operators";
+import {
+  MaterialesSonyInterface,
+  MaterialSonyInterface
+} from "src/app/models/materialesSony";
+import { NgbCarousel, NgbCarouselConfig } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-sony-materiales',
-  templateUrl: './sony-materiales.component.html',
-  styleUrls: ['./sony-materiales.component.css']
+  selector: "app-sony-materiales",
+  templateUrl: "./sony-materiales.component.html",
+  styleUrls: ["./sony-materiales.component.css"]
 })
 export class SonyMaterialesComponent implements OnInit {
- 
   slides: any = [[]];
   chunk(arr, chunkSize) {
     let R = [];
@@ -25,24 +27,27 @@ export class SonyMaterialesComponent implements OnInit {
   }
 
   pageActual = 1;
-  searchTerm : string;
+  searchTerm: string;
 
-  constructor( private dataApi: DataApiService, private authService: AuthService,
-    config: NgbCarouselConfig ) { 
-      config.interval = 5000;
-      config.wrap = true;
-      config.keyboard = true;
-      config.pauseOnHover = true;
-      config.showNavigationArrows = true;
-      config.showNavigationIndicators = true; 
-      
-
-    }
+  constructor(
+    private dataApi: DataApiService,
+    private authService: AuthService,
+    config: NgbCarouselConfig
+  ) {
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+  }
 
   public materialesSony: MaterialesSonyInterface[];
   public isAdmin: any = null;
   public userUid: string = null;
   private user: UserInterface;
+  materiales: Array<Object>;
+  cines: Array<Object>;
 
   ngOnInit() {
     this.getAllMaterialesSony();
@@ -50,7 +55,48 @@ export class SonyMaterialesComponent implements OnInit {
     //this.slides = this.chunk(this.getAllMaterialesSony, 3);
   }
 
- 
+  filterByMaterial(material) {
+    this.dataApi
+      .getAllMaterialesSonyList()
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      )
+      .subscribe(materialesSony => {
+        let filtered = [];
+        for (let i = 0; i < materialesSony.length; i++) {
+          if (materialesSony[i].MaterialType == material) {
+            filtered.push(materialesSony[i]);
+          }
+        }
+
+        this.materialesSony = filtered;
+      });
+  }
+
+  filterByCine(cine) {
+    this.dataApi
+      .getAllMaterialesSonyList()
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      )
+      .subscribe(materialesSony => {
+        let filtered = [];
+        for (let i = 0; i < materialesSony.length; i++) {
+          if (materialesSony[i].Theater == cine) {
+            filtered.push(materialesSony[i]);
+          }
+        }
+
+        this.materialesSony = filtered;
+      });
+  }
+
   getCurrentUser() {
     this.authService.isAuth().subscribe(user => {
       if (user) {
@@ -58,38 +104,49 @@ export class SonyMaterialesComponent implements OnInit {
         this.user.email = user.email;
         this.user.photoUrl = user.photoURL;
       }
-    })
+    });
   }
 
   //---------- Using RealTime database -------------------
 
   getAllMaterialesSony() {
-    this.dataApi.getAllMaterialesSonyList().snapshotChanges().pipe(
-      map(changes =>
-        changes.map(c =>
-          ({ key: c.payload.key, ...c.payload.val() })
+    this.dataApi
+      .getAllMaterialesSonyList()
+      .snapshotChanges()
+      .pipe(
+        map(changes =>
+          changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
         )
       )
-    ).subscribe(materialesSony => {
-      this.materialesSony = materialesSony;
-      console.log("materialesSony", this.materialesSony);
-    });
+      .subscribe(materialesSony => {
+        this.materialesSony = materialesSony;
+
+        //get an array with all the materials
+        this.materiales = this.materialesSony.map(function(item) {
+          return item["MaterialType"];
+        });
+        this.materiales = [...new Set(this.materiales)];
+
+        //get an array with all the theathers
+        this.cines = this.materialesSony.map(function(item) {
+          return item["Theater"];
+        });
+        this.cines = [...new Set(this.cines)];
+      });
   }
 
-  deleteMaterialSony(materialSonyKey: string){
+  deleteMaterialSony(materialSonyKey: string) {
     console.log("SELECTED", materialSonyKey);
-    const confirmacion = confirm('Are you sure?');
-    if (confirmacion){
-      this.dataApi.deleteMaterialSony(materialSonyKey).catch(err => console.log(err));
+    const confirmacion = confirm("Are you sure?");
+    if (confirmacion) {
+      this.dataApi
+        .deleteMaterialSony(materialSonyKey)
+        .catch(err => console.log(err));
     }
-  
   }
 
-  onPreUpdateMaterialSony(materialSony: MaterialSonyInterface){
-    console.log("ON PRE UPGRADE", materialSony)
+  onPreUpdateMaterialSony(materialSony: MaterialSonyInterface) {
+    console.log("ON PRE UPGRADE", materialSony);
     this.dataApi.selectedSonyMaterial = Object.assign({}, materialSony);
   }
-
-  
 }
-
