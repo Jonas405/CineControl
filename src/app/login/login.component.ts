349233e6fee0ef1638fd5dc5f25829f457216bd0
cristian97r <cristian97drb@gmail.com>
2019-12-26 17:27:24 -0400

    add alert for wrong login credentials

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,39 +1,44 @@
-import { Component, OnInit, Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
-import { AuthService } from '../services/auth.service';
-
+import { Component, OnInit, Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "../services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
-
 export class LoginComponent implements OnInit {
-
   rememberMe: boolean = false;
 
-  constructor( public afAuth: AngularFireAuth, private router: Router, private authService: AuthService) { }
-  public email: string = '';
-  public password: string = '';
-  ngOnInit() {
-  }
+  constructor(
+    public afAuth: AngularFireAuth,
+    private router: Router,
+    private authService: AuthService
+  ) {}
+  public email: string = "";
+  public password: string = "";
+  wrong = false;
+  ngOnInit() {}
 
   onLogIn(f: NgForm): void {
-    console.log('email',this.email);
-    console.log('password',this.password);
-    this.authService.loginEmailUser(this.email, this.password)
-      .then((res) => {
-        console.log('resUser',res);
+    console.log("email", this.email);
+    console.log("password", this.password);
+    this.authService
+      .loginEmailUser(this.email, this.password)
+      .then(res => {
+        console.log("resUser", res);
         this.onLoginRedirect();
-      }).catch(err => console.log('err', err.message));
+      })
+      .catch(err => {
+        console.log("err", err.message);
+        this.wrong = true;
+      });
   }
 
   onLoginRedirect(): void {
-    this.router.navigate(['/cinema']);
+    this.router.navigate(["/cinema"]);
   }
-
 }
