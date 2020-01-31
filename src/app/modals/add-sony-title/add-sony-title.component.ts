bd1693d2077e1a3431d0b4c1b6229640a9100503
Jonas405 <jonasleon405@gmail.com>
2020-01-31 16:16:12 -0600

    Cine control v1 31-01-2020

--- src/app/modals/add-sony-title/add-sony-title.component.ts
+++ src/app/modals/add-sony-title/add-sony-title.component.ts
@@ -133,6 +133,19 @@ export class AddSonyTitleComponent implements OnInit {
     }
   }
 
+  showPreview(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.imgSrc = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    }
+    else {
+      this.imgSrc = '/assets/img/image_placeholder.jpg';
+      this.selectedImage = null;
+    }
+   }
+
   ngOnInit(): void {}
 }
 
